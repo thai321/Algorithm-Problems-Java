5628fb27176a11b5233b029a53aa23a4e839a016
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-19 18:55:38 -0700

    Fix readme

--- Strongly Connected Components (SCC)/KosarajuAlgorithm.java
+++ Strongly Connected Components (SCC)/KosarajuAlgorithm.java
@@ -47,51 +47,50 @@ public class KosarajuAlgorithm {
   public static void main(String[] args) {
     List<Vertex> vertexList = new ArrayList<>();
 
-		vertexList.add(new Vertex(0, "a"));
-		vertexList.add(new Vertex(1, "b"));
-		vertexList.add(new Vertex(2, "c"));
-		vertexList.add(new Vertex(3, "d"));
-		vertexList.add(new Vertex(4, "e"));
-		vertexList.add(new Vertex(5, "f"));
-		vertexList.add(new Vertex(6, "g"));
-		vertexList.add(new Vertex(7, "h"));
+    vertexList.add(new Vertex(0, "a"));
+    vertexList.add(new Vertex(1, "b"));
+    vertexList.add(new Vertex(2, "c"));
+    vertexList.add(new Vertex(3, "d"));
+    vertexList.add(new Vertex(4, "e"));
+    vertexList.add(new Vertex(5, "f"));
+    vertexList.add(new Vertex(6, "g"));
+    vertexList.add(new Vertex(7, "h"));
 
     // All edges's weight is 1
-		List<Edge> edgeList = new ArrayList<Edge>();
+    List<Edge> edgeList = new ArrayList<Edge>();
 
-		edgeList.add(new Edge(1, vertexList.get(0), vertexList.get(1)));
+    edgeList.add(new Edge(1, vertexList.get(0), vertexList.get(1)));
 
-		edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(2)));
-		edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(4)));
-		edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(5)));
+    edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(2)));
+    edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(4)));
+    edgeList.add(new Edge(1, vertexList.get(1), vertexList.get(5)));
 
-		edgeList.add(new Edge(1, vertexList.get(2), vertexList.get(3)));
-		edgeList.add(new Edge(1, vertexList.get(2), vertexList.get(6)));
+    edgeList.add(new Edge(1, vertexList.get(2), vertexList.get(3)));
+    edgeList.add(new Edge(1, vertexList.get(2), vertexList.get(6)));
 
-		edgeList.add(new Edge(1, vertexList.get(3), vertexList.get(2)));
-		edgeList.add(new Edge(1, vertexList.get(3), vertexList.get(7)));
+    edgeList.add(new Edge(1, vertexList.get(3), vertexList.get(2)));
+    edgeList.add(new Edge(1, vertexList.get(3), vertexList.get(7)));
 
-		edgeList.add(new Edge(1, vertexList.get(4), vertexList.get(0)));
-		edgeList.add(new Edge(1, vertexList.get(4), vertexList.get(5)));
+    edgeList.add(new Edge(1, vertexList.get(4), vertexList.get(0)));
+    edgeList.add(new Edge(1, vertexList.get(4), vertexList.get(5)));
 
-		edgeList.add(new Edge(1, vertexList.get(5), vertexList.get(6)));
+    edgeList.add(new Edge(1, vertexList.get(5), vertexList.get(6)));
 
-		edgeList.add(new Edge(1, vertexList.get(6), vertexList.get(5)));
+    edgeList.add(new Edge(1, vertexList.get(6), vertexList.get(5)));
 
-		edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(3)));
-		edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(6)));
+    edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(3)));
+    edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(6)));
 
 
 
-		Graph graph = new Graph(vertexList,edgeList);
+    Graph graph = new Graph(vertexList,edgeList);
 
     KosarajuAlgorithm kosarajuAlgorithm = new KosarajuAlgorithm(graph);
 
-		System.out.println(kosarajuAlgorithm.getCount());
+    System.out.println(kosarajuAlgorithm.getCount());
 
-		for(Vertex vertex : vertexList){
-			System.out.println(vertex+" - "+vertex.getComponentId());
-		}
+    for(Vertex vertex : vertexList)
+    	System.out.println(vertex+" - "+vertex.getComponentId());
   }
 }
 /*
